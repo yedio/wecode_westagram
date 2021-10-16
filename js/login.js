94964d55eb220b1f4103ae9e77412035895171ea
yedio <yediobjet@gmail.com>
2021-10-16 15:54:58 +0900

    ADD : login.js 유효성 검사 추가 - ID : @ 포함 유무 - PW : 5 글자 이상

--- js/login.js
+++ js/login.js
@@ -4,20 +4,19 @@ const loginPw = document.getElementById('LOGIN_PW');
 const loginBtn = document.getElementById('LOGIN_BTN');
 
 function color() {
-    if(loginId.value.length>0 && loginPw.value.length>0){
+    if((loginId.value.length>0 && loginId.value.indexOf("@")!==-1) 
+        && loginPw.value.length>=5){
         loginBtn.style.backgroundColor = "#0095F6";
         loginBtn.disabled = false;
     }else{
-        loginBtn.style.backgroundColor = "#C0DFFD"
+        loginBtn.style.backgroundColor = "#C0DFFD";
         loginBtn.disabled = true;
     }
 }
 
 loginId.addEventListener('keyup', color);
 loginPw.addEventListener('keyup', color);
-loginBtn.addEventListener('click', (event) => {
-    console.log('버튼이 활성화되어 있습니다.');
-})
+ 
 
 
 
