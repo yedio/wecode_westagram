9d95516e733dc073547c7b09df77c2ac9b30e696
yedio <yediobjet@gmail.com>
2021-10-16 12:09:19 +0900

    main 페이지 레이아웃 1차 가닥

--- js/login.js
+++ js/login.js
@@ -6,13 +6,18 @@ const loginBtn = document.getElementById('LOGIN_BTN');
 function color() {
     if(loginId.value.length>0 && loginPw.value.length>0){
         loginBtn.style.backgroundColor = "#0095F6";
+        loginBtn.disabled = false;
     }else{
         loginBtn.style.backgroundColor = "#C0DFFD"
+        loginBtn.disabled = true;
     }
 }
 
 loginId.addEventListener('keyup', color);
 loginPw.addEventListener('keyup', color);
+loginBtn.addEventListener('click', (event) => {
+    console.log('버튼이 활성화되어 있습니다.');
+})
 
 
 
