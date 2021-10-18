b417b31674f57ad6b1324d8100524c702afca355
yedio <yediobjet@gmail.com>
2021-10-19 01:24:23 +0900

    main페이지 댓글추가기능 수정

--- js/main.js
+++ js/main.js
@@ -3,21 +3,45 @@
 /* Mission4 - 댓글 게시 기능 */
 const feedCmt = document.getElementById('feedComment');
 const feedBtn = document.getElementById('feedButton');
-const cmtUp = document.getElementById('CMT_WRITE');
 
 function btnColor() {
-    if(feedCmt.value.length>0){
-        feedBtn.style.color="#0095F6";
-        feedBtn.disabled = false;
-    }else{
+    if(!feedCmt.value.length){
         feedBtn.style.color = "#B8E2FD";
-        feedBtn.disabled = 'disabled';
+    }else{
+        feedBtn.style.color="#0095F6";
     }
 }
 
-function commentUp(){
-    cmtUp.innerHTML+='<div><span><strong>yestagram </strong></span>'+feedCmt.value+'</div>';
+function commentCheck(){
+    if(!feedCmt.value.length){
+        alert("댓글을 입력하시오");
+    }else{
+        commentUp(feedCmt.value);
+        
+    }
 }
 
-feedCmt.addEventListener('keyup', btnColor);
-feedBtn.addEventListener('click',commentUp);
+function commentUp(value){
+    const commentLists = document.getElementById("CMT_WRITE");
+    const newCommentList = document.createElement("li");
+    const newComment = `<span class="bold">yestagram </span><span>${value}</span><span><span class="delete">x</span>`
+
+    newCommentList.innerHTML = newComment;
+    deleteComment(newCommentList);
+    commentLists.appendChild(newCommentList); //<ul>태그 안에 <li>코멘트 생성
+    feedCmt.value="" //댓글 초기화 
+    feedBtn.style.color = "#B8E2FD";
+}
+
+function deleteComment(newCommentList){
+    const deleteBtn = newCommentList.querySelector(".delete");
+    deleteBtn.addEventListener("click", () => newCommentList.remove());
+}
+
+
+const init = () => {
+    feedBtn.addEventListener("click",commentCheck);
+    feedCmt.addEventListener('keyup', btnColor);
+};
+
+init();
